c927411c3fa04203d28fd2bb5b69b66238c56b42
Misqke <misqke.rust@yahoo.com>
2022-01-23 13:03:10 -0500

    fix

--- scraper.js
+++ scraper.js
@@ -4,63 +4,62 @@ const puppeteer = require("puppeteer");
 const url = "https://orders.deanfoods.com/";
 
 const scraper = async (milkList, login, password) => {
-  try {
-    // start browser and open page
-    const browser = await puppeteer.launch({
-      defaultViewport: {
-        width: 300,
-        height: 500,
-      },
-    });
-    const page = await browser.newPage();
+  // start browser and open page
+  const browser = await puppeteer.launch({
+    defaultViewport: {
+      width: 300,
+      height: 500,
+    },
+  });
+  const page = await browser.newPage();
 
-    // send page to log in page
-    await page.goto(url);
+  // send page to log in page
+  await page.goto(url);
 
-    // log in, click inventory, click new
-    await page.type("#ProfileID", login);
-    await page.type("#AppPwd", password);
-    await page.keyboard.press("Enter");
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
-    await page.click("#listView div div.col-3.col-md-2.col-lg-1 span a");
-    await page.click("#addNew");
+  // log in, click inventory, click new
+  await page.type("#ProfileID", login);
+  await page.type("#AppPwd", password);
+  await page.keyboard.press("Enter");
+  await page.waitForNavigation({ waitUntil: "networkidle2" });
+  await page.click("#listView div div.col-3.col-md-2.col-lg-1 span a");
+  await page.click("#addNew");
 
-    // tab to set full inventory and submit
-    await page.keyboard.press("Tab", { delay: 500 });
-    await page.keyboard.press("Tab", { delay: 500 });
-    await page.keyboard.press("Tab", { delay: 600 });
-    await page.keyboard.press("Tab", { delay: 600 });
-    await page.keyboard.press("Space", { delay: 600 });
-    await page.keyboard.press("ArrowUp", { delay: 600 });
-    await page.keyboard.press("Enter", { delay: 600 });
+  // tab to set full inventory and submit
+  await page.keyboard.press("Tab", { delay: 500 });
+  await page.keyboard.press("Tab", { delay: 500 });
+  await page.keyboard.press("Tab", { delay: 600 });
+  await page.keyboard.press("Tab", { delay: 600 });
+  await page.keyboard.press("Space", { delay: 600 });
+  await page.keyboard.press("ArrowUp", { delay: 600 });
+  await page.keyboard.press("Enter", { delay: 600 });
+  await page.click(
+    "body  div.crp-body-content  div.crp-content  div  div  div  form  div:nth-child(3)  div:nth-child(8)  div  div  button"
+  );
+
+  // fill out inventory form from milk list, submit for review
+  await page.waitForSelector("tbody > tr > td > input");
+  for (let i = 0; i < milkList.length; i++) {
+    await page.type(
+      `#grouped-gridview > div.k-grid-content.k-auto-scrollable > table > tbody > tr:nth-child(${
+        i + 1
+      }) > td:nth-child(1) > input`,
+      `${milkList[i]}`
+    );
     await page.click(
-      "body  div.crp-body-content  div.crp-content  div  div  div  form  div:nth-child(3)  div:nth-child(8)  div  div  button"
+      `#grouped-gridview > div.k-grid-content.k-auto-scrollable > table > tbody > tr:nth-child(${
+        i + 1
+      }) > td:nth-child(2) > a > svg`
     );
+  }
+  await page.click("#action-review");
 
-    // fill out inventory form from milk list, submit for review
-    await page.waitForSelector("tbody > tr > td > input");
-    for (let i = 0; i < milkList.length; i++) {
-      await page.type(
-        `#grouped-gridview > div.k-grid-content.k-auto-scrollable > table > tbody > tr:nth-child(${
-          i + 1
-        }) > td:nth-child(1) > input`,
-        `${milkList[i]}`
-      );
-      await page.click(
-        `#grouped-gridview > div.k-grid-content.k-auto-scrollable > table > tbody > tr:nth-child(${
-          i + 1
-        }) > td:nth-child(2) > a > svg`
-      );
-    }
-    await page.click("#action-review");
+  // await review page
+  await page.waitForSelector(
+    ".well > div:nth-child(2) > div.k-grid-content > table > tbody > tr"
+  );
 
-    // await review page
-    await page.waitForSelector(
-      ".well > div:nth-child(2) > div.k-grid-content > table > tbody > tr"
-    );
-
-    // SUBMIT INVENTORY - PRODUCTION ONLY
-    /*
+  // SUBMIT INVENTORY - PRODUCTION ONLY
+  /*
     await page.keyboard.press("Tab", { delay: 500 });
     await page.keyboard.press("Tab", { delay: 500 });
     await page.keyboard.press("Tab", { delay: 600 });
@@ -68,17 +67,14 @@ const scraper = async (milkList, login, password) => {
     await page.keyboard.press("Enter", { delay: 400})
     */
 
-    // screenshot confirmation and encode in base64
-    const image = await page.screenshot({ type: "png", encoding: "base64" });
+  // screenshot confirmation and encode in base64
+  const image = await page.screenshot({ type: "png", encoding: "base64" });
 
-    // close browser
-    await browser.close();
+  // close browser
+  await browser.close();
 
-    // return base64 image
-    return image;
-  } catch (error) {
-    console.log(error);
-  }
+  // return base64 image
+  return image;
 };
 
 module.exports = scraper;
